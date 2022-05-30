6c61a764751a4958842fbbc216055dc2cb7481a9
WenyiLULU <luwenyi1991@gmail.com>
2022-05-30 15:21:07 +0200

    iteration 4 done

--- routes/drones.js
+++ routes/drones.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const Drone = require("../models/Drone.model")
+
 
 // require the Drone model here
+const Drone = require("../models/Drone.model")
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
   Drone.find()
     .then((drones)=>{
-      console.log('Get drones information:', drones)
+      //console.log('Get drones information:', drones)
       res.render('./drones/list', {drones})
     })
     .catch(error => {console.log(error)})
@@ -23,7 +24,7 @@ router.post('/drones/create', async (req, res, next) => {
   // Iteration #3: Add a new drone
   try{
     const newDrone = await Drone.create(req.body)
-    console.log('new drone created:', newDrone)
+    //console.log('new drone created:', newDrone)
     res.redirect('/drones')
   }catch (err){
     console.log('something went wrong: ', err)
@@ -33,12 +34,24 @@ router.post('/drones/create', async (req, res, next) => {
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  Drone.findById(req.params.id)
+    .then((drone)=>{
+      //console.log('Drone to edit is:', drone)
+      res.render('./drones/update-form', {drone})
+    })
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post('/drones/:id/edit', async (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  try{
+    
+    const updateDrone = await Drone.findByIdAndUpdate(req.params.id, req.body)
+    console.log('Dron edited:', updateDrone)
+    res.redirect('/drones')
+  }catch (err){
+    console.log('something went wrong: ', err)
+    res.render('./drones/update-form')
+  }
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
